8f98f0b87dd913bc6ba504c4674add7778b701ab
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2015-08-15 11:16:28 +0200

    Reorganization matrix util functions continued.

--- mymath/utility/mm_util.h
+++ mymath/utility/mm_util.h
@@ -9,51 +9,6 @@
 
 namespace mymath
 {
-#define MYMATH_UNPROJECT_FUNC(t) \
-  MYMATH_INLINE impl::vec3i<t> unproject( const impl::vec3i<t>& ndc_pos, const impl::mat4i<t>& inv_mvp ) \
-  { \
-    impl::vec4i<t> obj_space = inv_mvp * impl::vec4i<t>( ndc_pos, 1 ); \
-    return obj_space.xyz / obj_space.w; \
-  }
-
-#define MYMATH_CREATEROTATION_FUNC(t) \
-  MYMATH_INLINE impl::mat4i<t> create_rotation( const t& angle, const impl::vec3i<t>& vec ) \
-  { \
-    assert( !impl::is_eq( length( vec ), (t)0 ) ); \
-    t a = angle; \
-    t s = std::sin( a ); \
-    t c = std::cos( a ); \
-    impl::vec3i<t> v = normalize( vec ); \
-    t xx = v.x * v.x; \
-    t yy = v.y * v.y; \
-    t zz = v.z * v.z; \
-    t xy = v.x * v.y; \
-    t yz = v.y * v.z; \
-    t zx = v.z * v.x; \
-    t xs = v.x * s; \
-    t ys = v.y * s; \
-    t zs = v.z * s; \
-    t oc = ( t )1 - c; \
-    return impl::mat4i<t>( oc * xx + c, oc * xy + zs, oc * zx - ys, 0, \
-                           oc * xy - zs, oc * yy + c, oc * yz + xs, 0, \
-                           oc * zx + ys, oc * yz - xs, oc * zz + c, 0, \
-                           0, 0, 0, 1 ); \
-  }
-
-#define MYMATH_CREATESCALE_FUNC(t) \
-  MYMATH_INLINE impl::mat4i<t> create_scale( const impl::vec3i<t>& vec ) \
-  { return impl::mat4i<t>( vec.x, 0, 0, 0, \
-                           0, vec.y, 0, 0, \
-                           0, 0, vec.z, 0, \
-                           0, 0, 0, 1); }
-
-#define MYMATH_CREATETRANSLATION_FUNC(t) \
-  MYMATH_INLINE impl::mat4i<t> create_translation( const impl::vec3i<t>& vec ) \
-  { return impl::mat4i<t>( 1, 0, 0, 0, \
-                           0, 1, 0, 0, \
-                           0, 0, 1, 0, \
-                           vec.x, vec.y, vec.z, 1 ); }
-
 #define MYMATH_GETANGLE_FUNC(v, t) \
   MYMATH_INLINE t get_angle(const impl::v<t>& a, const impl::v<t>& b) \
   { return std::acos( dot( normalize(a), normalize(b) ) ); }
@@ -85,14 +40,6 @@ namespace mymath
     tangent = cross ( bitangent, normal ); \
     return normalize ( tangent ); \
   }
-
-  MYMATH_UNPROJECT_FUNC( float )
-
-    MYMATH_CREATEROTATION_FUNC( float )
-
-    MYMATH_CREATESCALE_FUNC( float )
-
-    MYMATH_CREATETRANSLATION_FUNC( float )
 
     MYMATH_GETANGLE_FUNC( vec2i, float )
     MYMATH_GETANGLE_FUNC( vec3i, float )
@@ -136,39 +83,6 @@ namespace mymath
     MYMATH_FINDNORMAL_FUNC( double )
     MYMATH_CALCTANGENT_FUNC( double )
 #endif
-
-    template< typename t >
-  MYMATH_INLINE impl::mat4i<t> perspective( const t& fovy, const t& aspect, const t& near, const t& far )
-  {
-    t top = near * std::tan( fovy * ( t )0.5 );
-    t bottom = -top;
-    t left = bottom * aspect;
-    t right = -left;
-    impl::mat4i<t> r = mat4::identity;
-    r[0].x = ( (t)2 * near ) / ( right - left );
-    r[1].y = ( (t)2 * near ) / ( top - bottom );
-    r[2].x = ( right + left ) / ( right - left );
-    r[2].y = ( top + bottom ) / ( top - bottom );
-    r[2].z = -( far + near ) / ( far - near );
-    r[2].w = -(t)1;
-    r[3].z = -( (t)2 * far * near ) / ( far - near );
-    r[3].w = (t)0;
-    return r;
-  }
-
-  template< typename t >
-  MYMATH_INLINE impl::mat4i<t> ortographic( const t& left, const t& right, const t& bottom, const t& top, const t& near, const t& far )
-  {
-    impl::mat4i<t> r = mat4::identity;
-    r[0].x = (t)2 / ( right - left );
-    r[1].y = (t)2 / ( top - bottom );
-    r[2].z = -(t)2 / ( far - near );
-    r[3].x = -( ( right + left ) / ( right - left ) );
-    r[3].y = -( ( top + bottom ) / ( top - bottom ) );
-    r[3].z = -( ( far + near ) / ( far - near ) );
-    r[3].w = (t)1;
-    return r;
-  }
 
   template< class t >
   MYMATH_INLINE float length_squared( const t& v )
--- mymath/utility/mm_util_matrix.h
+++ mymath/utility/mm_util_matrix.h
@@ -6,15 +6,98 @@
 
 namespace mymath
 {
+	#define MYMATH_UNPROJECT_FUNC(t) \
+	MYMATH_INLINE impl::vec3i<t> unproject( const impl::vec3i<t>& ndc_pos, const impl::mat4i<t>& inv_mvp ) \
+	{ \
+		impl::vec4i<t> obj_space = inv_mvp * impl::vec4i<t>( ndc_pos, 1 ); \
+		return obj_space.xyz / obj_space.w; \
+	}
+
+	#define MYMATH_CREATEROTATION_FUNC(t) \
+	MYMATH_INLINE impl::mat4i<t> create_rotation( const t& angle, const impl::vec3i<t>& vec ) \
+	{ \
+		assert( !impl::is_eq( length( vec ), (t)0 ) ); \
+		t a = angle; \
+		t s = std::sin( a ); \
+		t c = std::cos( a ); \
+		impl::vec3i<t> v = normalize( vec ); \
+		t xx = v.x * v.x; \
+		t yy = v.y * v.y; \
+		t zz = v.z * v.z; \
+		t xy = v.x * v.y; \
+		t yz = v.y * v.z; \
+		t zx = v.z * v.x; \
+		t xs = v.x * s; \
+		t ys = v.y * s; \
+		t zs = v.z * s; \
+		t oc = ( t )1 - c; \
+		return impl::mat4i<t>( oc * xx + c, oc * xy + zs, oc * zx - ys, 0, \
+								oc * xy - zs, oc * yy + c, oc * yz + xs, 0, \
+								oc * zx + ys, oc * yz - xs, oc * zz + c, 0, \
+								0, 0, 0, 1 ); \
+	}
+
+	#define MYMATH_CREATESCALE_FUNC(t) \
+	MYMATH_INLINE impl::mat4i<t> create_scale( const impl::vec3i<t>& vec ) \
+	{ return impl::mat4i<t>( vec.x, 0, 0, 0, \
+							0, vec.y, 0, 0, \
+							0, 0, vec.z, 0, \
+							0, 0, 0, 1); }
+
+	#define MYMATH_CREATETRANSLATION_FUNC(t) \
+	MYMATH_INLINE impl::mat4i<t> create_translation( const impl::vec3i<t>& vec ) \
+	{ return impl::mat4i<t>( 1, 0, 0, 0, \
+							0, 1, 0, 0, \
+							0, 0, 1, 0, \
+							vec.x, vec.y, vec.z, 1 ); }
+
+	MYMATH_UNPROJECT_FUNC(float)
+	MYMATH_CREATEROTATION_FUNC(float)
+	MYMATH_CREATESCALE_FUNC(float)
+	MYMATH_CREATETRANSLATION_FUNC(float)
+
+	template< typename t >
+	MYMATH_INLINE impl::mat4i<t> perspective(const t& fovy, const t& aspect, const t& near, const t& far)
+	{
+		t top = near * std::tan(fovy * (t)0.5);
+		t bottom = -top;
+		t left = bottom * aspect;
+		t right = -left;
+		impl::mat4i<t> r = mat4::identity;
+		r[0].x = ((t)2 * near) / (right - left);
+		r[1].y = ((t)2 * near) / (top - bottom);
+		r[2].x = (right + left) / (right - left);
+		r[2].y = (top + bottom) / (top - bottom);
+		r[2].z = -(far + near) / (far - near);
+		r[2].w = -(t)1;
+		r[3].z = -((t)2 * far * near) / (far - near);
+		r[3].w = (t)0;
+		return r;
+	}
+
+	template< typename t >
+	MYMATH_INLINE impl::mat4i<t> ortographic(const t& left, const t& right, const t& bottom, const t& top, const t& near, const t& far)
+	{
+		impl::mat4i<t> r = mat4::identity;
+		r[0].x = (t)2 / (right - left);
+		r[1].y = (t)2 / (top - bottom);
+		r[2].z = -(t)2 / (far - near);
+		r[3].x = -((right + left) / (right - left));
+		r[3].y = -((top + bottom) / (top - bottom));
+		r[3].z = -((far + near) / (far - near));
+		r[3].w = (t)1;
+		return r;
+	}
+
 	//TODO unit test
 	//this function transforms a position vector by a matrix
 	//it also ensures that the last coordinate of the vector will always be one
 	//ie. the vector returned will not be in homogenous coordinates
 	template< class t, class u >
-	MYMATH_INLINE t transform_position( const t& vector, const u& transformation)
+	MYMATH_INLINE t transform_position(const t& vector, const u& transformation)
 	{
 		t tmp = transformation * vector;
-		if( !impl::is_eq(tmp[tmp.length()-1], 1.0f) ) tmp /= tmp[tmp.length()-1];
+		if (!impl::is_eq(tmp[tmp.length() - 1], 1.0f)) tmp /= tmp[tmp.length() - 1];
 		return tmp;
 	}
 
@@ -23,7 +106,7 @@ namespace mymath
 	//it also ensures that the last coordinate of the vector will always be one
 	//because the last coordinate is implicitly zero (the vector represents a direction)
 	template< class t, class u >
-	MYMATH_INLINE t transform_direction( const t& vector, const u& transformation )
+	MYMATH_INLINE t transform_direction(const t& vector, const u& transformation)
 	{
 		t tmp = transformation * vector;
 		return tmp;
@@ -32,7 +115,7 @@ namespace mymath
 	//TODO unit test
 	//this function transforms a normal vector by the inverse transpose of a transformation matrix
 	//the matrix is assumed to contain the inverse transpose of the transformation
-	MYMATH_INLINE impl::vec3i<float> transform_direction( const impl::vec3i<float>& normal, const impl::mat4i<float>& inverse_transpose_transformation )
+	MYMATH_INLINE impl::vec3i<float> transform_direction(const impl::vec3i<float>& normal, const impl::mat4i<float>& inverse_transpose_transformation)
 	{
 		return impl::mat3i<float>(inverse_transpose_transformation) * normal;
 	}
@@ -42,7 +125,7 @@ namespace mymath
 	//1) apply a
 	//2) apply b
 	template< class t >
-	MYMATH_INLINE t concat_transformations( const t& a, const t& b )
+	MYMATH_INLINE t concat_transformations(const t& a, const t& b)
 	{
 		return b * a;
 	}
@@ -52,23 +135,23 @@ namespace mymath
 	//1) apply a
 	//2) apply b
 	template< class t >
-	MYMATH_INLINE t inv_concat_transformations( const t& inv_a, const t& inv_b )
+	MYMATH_INLINE t inv_concat_transformations(const t& inv_a, const t& inv_b)
 	{
 		return inv_a * inv_b;
 	}
 
 	//TODO unit test
 	//returns true if the transformation changes the handedness of the coordinate system
-	MYMATH_INLINE bool swaps_handedness( const impl::mat4i<float>& transformation )
+	MYMATH_INLINE bool swaps_handedness(const impl::mat4i<float>& transformation)
 	{
-		return determinant( impl::mat3i<float>(transformation) ) < 0;
+		return determinant(impl::mat3i<float>(transformation)) < 0;
 	}
 
-	//TODO test
+	//TODO unit test
 	template< typename t >
-	impl::mat4i<t> arbitrary_basis_matrix( const impl::mat4i<t>& x, const impl::mat4i<t>& y, const impl::mat4i<t>& z, const impl::mat4i<t>& origin )
+	impl::mat4i<t> arbitrary_basis_matrix(const impl::mat4i<t>& x, const impl::mat4i<t>& y, const impl::mat4i<t>& z, const impl::mat4i<t>& origin)
 	{
-		return impl::mat4i<t> (
+		return impl::mat4i<t>(
 			x.x, x.y, x.z, 0,
 			y.x, y.y, y.z, 0,
 			z.x, z.y, z.z, 0,
